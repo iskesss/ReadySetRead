0a0741ed54a34e0ada14664a96814c651cacb388
Marta Balik <mbalikcioglu@scu.edu>
2025-12-03 23:06:28 -0800

    rewards paeg revamp part 2

--- frontend/src/pages/Rewards.tsx
+++ frontend/src/pages/Rewards.tsx
@@ -1,74 +1,43 @@
-import { Link } from 'react-router-dom';
-
-
 //Component imports
 import { Button } from '../components/button';
 
 //Style imports
 import '../styles/Rewards.css'
 import '../styles/App.css'
-import Layout from "../components/layout";
-
-
-{/**
-    Places to connect to backend: 
-    1) getCoins -- for piggy bank box
-    2) when you cadd an incentive should be tracked on backend? 
-    3) 
-    */}
 
 export default function Rewards() {
-    return(
-        <div className='rewardsContainer'>
+  return (
+    <div className="rewardsPage">
 
+      <div className="container">
+        <div className="topRow">
+          <div className="card piggyCard">
+            <h2>Piggy Bank</h2>
+            <p>You have <span className="coinBubble">13</span> coins!</p>
+          </div>
 
-            <div className='topRow'>
-
-                {/* Piggy Bank Box */}
-                <div className="card squareCard">
-                    <h2> Piggy Bank</h2>
-                    <p> You have <span className="circleBackground">13</span> coins! </p>
-                </div>
-
-                {/* Parent Incentives Box */}
-                {/* need to connect parent card to backened getIncentives call? --  */}
-                <div className="card rectangleCard">
-                    <h2> Parent Incentives</h2>
-
-                    <div className='customIncentiveBox'>
-                        <p> Incentive: </p> {/* to print call from backend here?? */}
-
-                    </div>
-
-                    <div className='customIncentiveBox'>
-                        <p> Incentive: </p> {/* to print call from backend here?? */}
-                        
-                    </div>
-
-                    <div className='customIncentiveBox'>
-                        <p> Incentive: </p> {/* to print call from backend here?? */}
-                    </div>
-                </div>
-            </div>
-
-             <div className='bottomRow'>
-
-                {/* Store For Skins */}
-                <div className="card rectangleCard">
-                    <h2> Store: </h2>
-
-                     <div className='changeSkinBox'>
-                        <p> Dark app skin: 10 coins</p> 
-                    </div>
-
-                    <div className='changeSkinBox'>
-                        <p> Neon app skin: 10 coins</p> 
-                    </div>
-                </div>
-            
-            
-            </div>
-
+          <div className="card parentCard">
+            <h2>Parent Incentives</h2>
+            <Button className="miniReward">Ice cream: 25 coins</Button>
+            <Button className="miniReward">New soccer ball: 30 coins</Button>
+            <Button className="miniReward">New Xbox: 500 coins</Button>
+          </div>
         </div>
-    );
+
+        <div className="fullRow">
+          <div className="card storeCard">
+            <h2>Store</h2>
+
+            <div className="storeItemsRow">
+              <Button className="storeItem"> Dark app skin: 10 coins</Button>
+              <Button className="storeItem"> Neon app skin: 10 coins </Button>
+              <Button className="storeItem">  Jungle app skin: 20 coins </Button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
 }
