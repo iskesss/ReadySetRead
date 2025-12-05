f76d358a6fc30072714285067cf5687138c9c7db
Will DeGasparis <will.degasparis@gmail.com>
2025-12-04 20:59:47 -0800

    merge conflict fix on librarybook

--- frontend/src/components/LibraryBook.tsx
+++ frontend/src/components/LibraryBook.tsx
@@ -6,9 +6,8 @@ import "../styles/LibraryBook.css"
 
 
 type LibraryBookProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-<<<<<<< HEAD
     title: string;
-    status: string;
+    status: 'passed' | 'incomplete';
     level: number;
     added: boolean;
     onAdd: () => void;
@@ -16,41 +15,18 @@ type LibraryBookProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 // Commenting out old syntax so i remember, will probably need it on the non-dummy version
 // export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(({  title, status, level, onAdd ...props }, ref) => {
-export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(({ title, status, level, added, onAdd }) => {
-    return (
-        <div className='libraryBookCard'>
-            <h3>{title}</h3>
-            <p>Status: {status}</p>
-            <p>Level: {level}</p>
-            <Button onClick={onAdd}>{added ? "Added!" : "Add"}</Button>
-        </div>
-=======
 export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(
-  ({ title, status, level, onAdd }, ref) => {
-    const statusClass =
-      status === 'passed' ? 'passedCard' : 'incompleteCard';
+    ({ title, status, level, added, onAdd }) => {
+        const statusClass =
+            status === 'passed' ? 'passedCard' : 'incompleteCard';
 
-    return (
-      <div className={`libraryBookCard ${statusClass}`}>
-        <h3>{title}</h3>
-        <p>Status: {status}</p>
-        <p>Level: {level}</p>
-        <Button onClick={onAdd}>Add</Button>
-      </div>
->>>>>>> 96c92572597dd29790563aa9e0e02254942b1fc8
-=======
-export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(
-  ({ title, status, level, onAdd }, ref) => {
-    const statusClass =
-      status === 'passed' ? 'passedCard' : 'incompleteCard';
-
-    return (
-      <div className={`libraryBookCard ${statusClass}`}>
-        <h3>{title}</h3>
-        <p>Status: {status}</p>
-        <p>Level: {level}</p>
-        <Button onClick={onAdd}>Add</Button>
-      </div>
->>>>>>> 96c92572597dd29790563aa9e0e02254942b1fc8
-    );
-})
+        return (
+            <div className={`libraryBookCard ${statusClass}`}>
+                <h3>{title}</h3>
+                <p>Status: {status}</p>
+                <p>Level: {level}</p>
+                <Button onClick={onAdd}>{added ? "Added!" : "Add"}</Button>
+            </div>
+        );
+    }
+);
