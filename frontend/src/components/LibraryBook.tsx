cead153deae53a43832da886678a9d7e75d82010
Juliana Bruno <jjulianabrunoo@gmail.com>
2025-12-04 20:38:33 -0800

    css changes for complete book

--- frontend/src/components/LibraryBook.tsx
+++ frontend/src/components/LibraryBook.tsx
@@ -6,21 +6,26 @@ import "../styles/LibraryBook.css"
 
 
 type LibraryBookProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    title: string;
-    status: string;
-    level: number;
-    onAdd: () => void;
+  title: string;
+  status: 'passed' | 'incomplete';
+  level: number;
+  onAdd: () => void;
 };
 
 // Commenting out old syntax so i remember, will probably need it on the non-dummy version
 // export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(({  title, status, level, onAdd ...props }, ref) => {
-export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(({ title, status, level, onAdd }) => {
+export const LibraryBook = forwardRef<HTMLButtonElement, LibraryBookProps>(
+  ({ title, status, level, onAdd }, ref) => {
+    const statusClass =
+      status === 'passed' ? 'passedCard' : 'incompleteCard';
+
     return (
-        <div className='libraryBookCard'>
-            <h3>{title}</h3>
-            <p>Status: {status}</p>
-            <p>Level: {level}</p>
-            <Button onClick={onAdd}>Add</Button>
-        </div>
+      <div className={`libraryBookCard ${statusClass}`}>
+        <h3>{title}</h3>
+        <p>Status: {status}</p>
+        <p>Level: {level}</p>
+        <Button onClick={onAdd}>Add</Button>
+      </div>
     );
-})
+  }
+);
